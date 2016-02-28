ddd13d280a989d25520d2266bbf27623947c5bf7
Olli Korhonen <olli.korhonen@iki.fi>
2016-02-28 18:34:55 +0200

    fanout -> direct

--- hsl.js
+++ hsl.js
@@ -3,11 +3,11 @@ var hslclient  = mqtt.connect('mqtt://213.138.147.225:1883/');
 
 var amqp = require('amqplib/callback_api')
 
-amqp.connect('amqp://localhost', function(err, conn) {
+amqp.connect('amqp://192.168.0.2', function(err, conn) {
     conn.createChannel(function(err, ch) {
         var ex = 'hsl_exchange';
 
-        ch.assertExchange(ex, 'fanout', {durable: false});
+        ch.assertExchange(ex, 'direct', {durable: false});
 
         hslclient.on('connect', function () {
             var subs = '/hfp/journey/tram/+/1009/#';
@@ -22,7 +22,7 @@ amqp.connect('amqp://localhost', function(err, conn) {
             message = JSON.parse(message);
             message.topic = topic;
 
-            ch.publish(ex, '', new Buffer(JSON.stringify(message)));
+            ch.publish(ex, 'vehicledata', new Buffer(JSON.stringify(message)));
         });
     });
 });
